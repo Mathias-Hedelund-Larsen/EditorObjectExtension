f589b5f6e4e00efed2d91afdd9441fa2d07ab3b0
MathDodo <mathias.h.larsen@jp.dk>
2020-03-21 14:56:22 +0100

    Commented code and all extensions for serializeproperty and unityengine.object for no

---
+++ Assets/HephaestusForge/Editor/Extensions/SerializedProperyExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace HephaestusForge
+{
+    /// <summary> 
+    /// Extensions for the SerializedProperty class
+    /// </summary>
+    public static class SerializedProperyExtensions
+    {
+        /// <summary>
+        /// Gets all children of SerializedProperty at 1 level depth.
+        /// </summary>
+        /// <param name="serializedProperty">Parent SerializedProperty.</param>
+        /// <returns>Collection of `SerializedProperty` children.</returns>
+        public static IEnumerable<SerializedProperty> GetChildren(this SerializedProperty serializedProperty)
+        {
+            SerializedProperty currentProperty = serializedProperty.Copy();
+            SerializedProperty nextSiblingProperty = serializedProperty.Copy();
+            {
+                nextSiblingProperty.Next(false);
+            }
+
+            if (currentProperty.Next(true))
+            {
+                do
+                {
+                    if (SerializedProperty.EqualContents(currentProperty, nextSiblingProperty))
+                        break;
+
+                    yield return currentProperty;
+                }
+                while (currentProperty.Next(false));
+            }
+        }
+
+        /// <summary>
+        /// Gets visible children of `SerializedProperty` at 1 level depth.
+        /// </summary>
+        /// <param name="serializedProperty">Parent SerializedProperty.</param>
+        /// <returns>Collection of `SerializedProperty` children.</returns>
+        public static IEnumerable<SerializedProperty> GetVisibleChildren(this SerializedProperty serializedProperty)
+        {
+            SerializedProperty currentProperty = serializedProperty.Copy();
+
+            if (currentProperty.NextVisible(true))
+            {
+                do
+                {
+                    if (currentProperty.propertyPath.Contains(serializedProperty.propertyPath))
+                    {
+                        yield return currentProperty.Copy();
+                    }
+                }
+                while (currentProperty.NextVisible(false));
+            }
+        }
+
+        /// <summary>
+        /// Search through an SerializedProperty array to find a specific child property defined by the predicate,
+        /// you will also get the index of the child property inside the array.
+        /// </summary>
+        /// <param name="array">The source SerializedProperty which needs to be an array.</param>
+        /// <param name="predicate">Definitions of the value to search for in the array.</param>
+        /// <param name="index">The index of the found value, -1 if the value couldnt be found.</param>
+        /// <returns>The found SerializedProperty, will return the array if the child property couldnt be found.</returns>
+        public static SerializedProperty FindInArray(this SerializedProperty array, Predicate<SerializedProperty> predicate, out int index)
+        {
+            if (array.isArray)
+            {
+                for (int i = array.arraySize - 1; i >= 0; i--)
+                {
+                    if (predicate.Invoke(array.GetArrayElementAtIndex(i)))
+                    {
+                        index = i;
+                        return array.GetArrayElementAtIndex(i);
+                    }
+                }
+
+                Debug.LogWarning("Couldnt find what you were searching for in array");
+                index = -1;
+                return array;
+            }
+            else
+            {
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+                Debug.LogError("The serialized property was not an array");
+#endif
+
+                index = -1;
+                return array;
+            }
+        }
+    }
+}
---
+++ Assets/HephaestusForge/Editor/Extensions/UnityEditorObjectExtensions.cs
@@ -0,0 +1,114 @@
+﻿using UnityEditor;
+using System.Linq;
+using UnityEngine;
+using System.Reflection;
+using UnityEditor.SceneManagement;
+using UnityEditor.Experimental.SceneManagement;
+
+namespace HephaestusForge
+{
+    /// <summary>
+    /// Extensions for the UnityEngine.Object only available in the Editor
+    /// </summary>
+    public static class UnityEditorObjectExtensions
+    {
+        //Using reflection to get a method for finding objects by an instance id.
+        private static MethodInfo _getObjectByInstanceID = typeof(Object).GetMethod("FindObjectFromInstanceID", BindingFlags.NonPublic | BindingFlags.Static);
+
+        //Using reflection to be able to use values form the inspectorMode, where local file ID is accesible
+        private static PropertyInfo _inspectorModeInfo = typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        /// <summary>
+        /// Extension method to get a sceneGuid and objec id from a UnityEngine.Object, the sceneGuid will be None if it is an asset or prefab.
+        /// The objectID will be the InstanceID if the Object is an asset or prefab, and will be the local file id if it is inside a scene.
+        /// </summary>
+        /// <param name="source">The source object for the extension.</param>
+        /// <param name="sceneGuid">The guid of the scene to get.</param>
+        /// <param name="objectID">The id of the object to get.</param>
+        public static void GetSceneGuidAndObjectID(this Object source, out string sceneGuid, out int objectID)
+        {
+            if (AssetDatabase.Contains(source))
+            {
+                sceneGuid = "None";
+                objectID = source.GetInstanceID();
+            }
+            else if (PrefabStageUtility.GetCurrentPrefabStage() != null)
+            {
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(PrefabStageUtility.GetCurrentPrefabStage().prefabAssetPath);
+                var components = prefab.GetComponents(source.GetType()).ToList();
+                components.AddRange(prefab.GetComponentsInChildren(source.GetType()));
+
+                sceneGuid = "None";
+                int localID = source.GetLocalID();
+                objectID = localID;
+
+                for (int i = 0; i < components.Count; i++)
+                {
+                    if (localID == components[i].GetLocalID())
+                    {
+                        objectID = components[i].GetInstanceID();
+                    }
+                }
+            }
+            else
+            {
+                var scene = (source as Component).gameObject.scene;
+                sceneGuid = AssetDatabase.AssetPathToGUID(scene.path);
+
+                objectID = source.GetLocalID();
+
+                if (objectID == 0)
+                {
+                    if (EditorSceneManager.SaveScene(scene))
+                    {
+                        objectID = source.GetLocalID();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Getting the local file id of and object.
+        /// </summary>
+        /// <param name="source">The source object to get the local file id.</param>
+        /// <returns>The local file id.</returns>
+        public static int GetLocalID(this Object source)
+        {
+            SerializedObject serializedObject = new SerializedObject(source);
+            _inspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
+
+            SerializedProperty localIdProp = serializedObject.FindProperty("m_LocalIdentfierInFile");
+
+            return localIdProp.intValue;
+        }
+
+        /// <summary>
+        /// Get the MonoScript of the object.
+        /// </summary>
+        /// <param name="source">The source object to get the MonoScript from.</param>
+        /// <returns>A MonoScript which contains the class for the sourceobject.</returns>
+        public static MonoScript GetScript(this Object source)
+        {
+            if (source is MonoBehaviour)
+            {
+                return MonoScript.FromMonoBehaviour((MonoBehaviour)source);
+            }
+            else if (source is ScriptableObject)
+            {
+                return MonoScript.FromScriptableObject((ScriptableObject)source);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Find an object by an instanceID.
+        /// </summary>
+        /// <param name="instanceID">The instance id of the object you want to find.</param>
+        /// <returns>The object found from the instanceID.</returns>
+        public static Object GetObjectByInstanceID(int instanceID)
+        {
+            return (Object)_getObjectByInstanceID.Invoke(null, new object[] { instanceID });
+        }
+    }
+}
